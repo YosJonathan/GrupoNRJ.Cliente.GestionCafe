e12ca24ace83ded64ee2295c8e3720554c5684dc
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-10-24 22:29:32 -0600

    [jyos][Creando autenticacion jwt bearer]

--- GrupoNRJ.Cliente.GestionCafe/GrupoNRJ.Cliente.GestionCafe/Utilidades/ClienteAPI.cs
+++ GrupoNRJ.Cliente.GestionCafe/GrupoNRJ.Cliente.GestionCafe/Utilidades/ClienteAPI.cs
@@ -4,8 +4,11 @@
 
 namespace GrupoNRJ.Cliente.GestionCafe.Utilidades
 {
+    using GrupoNRJ.Modelos.GestionCafe.Solicitudes;
+    using System.Net.Http.Headers;
     using System.Text;
     using System.Text.Json;
+    using static System.Net.WebRequestMethods;
 
     /// <summary>
     /// Clase para api de clientes.
@@ -14,6 +17,7 @@ namespace GrupoNRJ.Cliente.GestionCafe.Utilidades
     {
         private readonly HttpClient httpClient;
         private readonly JsonSerializerOptions jsonOptions;
+        private string token;
 
         public ClienteAPI(HttpClient httpClient)
         {
@@ -32,6 +36,13 @@ namespace GrupoNRJ.Cliente.GestionCafe.Utilidades
         /// <returns>Instancia del modelo con los datos de la API.</returns>
         public async Task<T?> GetAsync<T>(string url)
         {
+            if (string.IsNullOrEmpty(this.token))
+            {
+                this.token = await this.ObtenerTokenAsync("root", "root");
+            }
+
+            this.httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.token);
+
             var response = await this.httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<T>(this.jsonOptions);
@@ -47,6 +58,13 @@ namespace GrupoNRJ.Cliente.GestionCafe.Utilidades
         /// <returns>Instancia del modelo con los datos devueltos por la API.</returns>
         public async Task<TResponse?> PostAsync<TRequest, TResponse>(string url, TRequest? datos)
         {
+            if (string.IsNullOrEmpty(this.token))
+            {
+                this.token = await this.ObtenerTokenAsync("root", "root");
+            }
+
+            this.httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.token);
+
             var json = JsonSerializer.Serialize(datos, this.jsonOptions);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
@@ -84,6 +102,36 @@ namespace GrupoNRJ.Cliente.GestionCafe.Utilidades
         {
             var response = await this.httpClient.DeleteAsync(url);
             return response.IsSuccessStatusCode;
+        }
+
+        /// <summary>
+        /// Obtiene token async
+        /// </summary>
+        /// <param name="usuario">Usuario.</param>
+        /// <param name="clave">Contraseña.</param>
+        /// <returns>token valido.</returns>
+        /// <exception cref="Exception">No se pudo autenticar.</exception>
+        private async Task<string> ObtenerTokenAsync(string usuario, string clave)
+        {
+#pragma warning disable SA1118 // Parameter must not span multiple lines
+            var response = await this.httpClient.PostAsJsonAsync("/Sevicio.GestionCafe/api/Auth/login", new
+            LoginSolicitud
+            {
+                Username = usuario,
+                Password = clave
+            });
+#pragma warning restore SA1118 // Parameter must not span multiple lines
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("No se pudo autenticar");
+            }
+
+            var json = await response.Content.ReadFromJsonAsync<JsonElement>();
+#pragma warning disable CS8603 // Posible tipo de valor devuelto de referencia nulo
+            return json.GetProperty("token")
+                .GetString();
+#pragma warning restore CS8603 // Posible tipo de valor devuelto de referencia nulo
         }
     }
 }
